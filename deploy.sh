23d5b913b48295f184172042b638355d3adcf44f
ramdsc <adr.her.arc.95@gmail.com>
2018-11-17 00:50:00 +0100

    Deployment Bash script v2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cd /d/home/site/repository/backend &&\
-npm install &&\
-nohup npm start & &&\
+cd /d/home/site/repository/backend
+npm install
+nohup npm start &
 cp -rs /d/home/site/repository/frontend/* /d/home/site/wwwroot/
