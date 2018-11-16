e4079cfda1862874bf787b4f17c3e5657119e4c8
ramdsc <adr.her.arc.95@gmail.com>
2018-11-16 12:19:19 +0100

    Cleaned up, included Express as a dependency

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const angular = require('angular');
+const express = require('express');
+const app = express();
+let port = process.env.port || 3000;
+app.get('/', (req, res) => res.send('Hello World'));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
