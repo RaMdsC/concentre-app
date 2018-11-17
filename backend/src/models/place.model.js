a74813c5aa73aaca4381208a4dbb9b052be148eb
ramdsc <adr.her.arc.95@gmail.com>
2018-11-17 04:03:05 +0100

    Clean models

--- backend/src/models/place.model.js
+++ backend/src/models/place.model.js
@@ -4,15 +4,30 @@ const env = require('../env');
 mongoose.connect(env.db);
 
 const PlaceSchema = new mongoose.Schema({
-    idPlace: Number,
+    idPlace: {
+        default: env.randomId(),
+        type: String
+    },
     capacity: {
         required: true,
         type: Number
     },
-    dateAdded: Date,
-    description: String,
-    distance: Number,
-    imagePath: String,
+    dateAdded: {
+        default: new Date(),
+        type: Date
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    distance: {
+        required: true,
+        type: Number
+    },
+    imagePath: {
+        required: true,
+        type: String
+    },
     owner: {
         required: true,
         type: String
@@ -21,7 +36,10 @@ const PlaceSchema = new mongoose.Schema({
         required: true,
         type: Number
     },
-    rating: Number
+    rating: {
+        default: 0,
+        type: Number
+    }
 });
 
 module.exports = mongoose.model('Place', PlaceSchema);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,12 +4,27 @@ const env = require('../env');
 mongoose.connect(env.db);
 
 const UserSchema = new mongoose.Schema({
+    energyPoints: {
+        default: 0,
+        type: Number
+    },
+    imagePath: {
+        required: true,
+        type: String
+    },
+    name: {
+        required: true,
+        type: String
+    },
+    surname: {
+        required: true,
+        type: String
+    },
     userName: {
         required: true,
         type: String,
         unique: true
-    },
-    energyPoints: Number
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
